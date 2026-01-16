2d4a2d93e2c6f1c156170e9d79af0df563864ece
Marius <mariussangwa@gmail.com>
2026-01-16 16:38:24 +0200

    setting the public folder to static

--- server.js
+++ server.js
@@ -1,29 +1,15 @@
 import express from 'express';
 import path from 'path';
-import url from 'url'
+import url from 'url';
+
 const app = express();
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'public','index.html'))
-})
+// setUp static folder
 
-app.get('/crops.html', (req,res)=>{
-    res.sendFile(path.join(__dirname,'public','crops.html'))
-})
-
-app.get('/contact.html', (req,res)=>{
-    res.sendFile(path.join(__dirname,'public','contact.html'))
-})
-
-app.get('/index.html', (req,res)=>{
-    res.sendFile(path.join(__dirname,'public','index.html'))
-})
-
-
-
+app.use(express.static(path.join(__dirname ,'public')))
 
 app.listen(7000,()=>{
     console.log(`sever running on port 7000`)
