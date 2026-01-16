eb06d83df8deecb8f2856a01f64ccf6f5ccf4922
Marius <mariussangwa@gmail.com>
2026-01-16 16:32:27 +0200

    updating the public folder to contain index crops and the contact pages

--- server.js
+++ server.js
@@ -7,12 +7,22 @@ const __dirname = path.dirname(__filename)
 
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'public','home.html'))
+    res.sendFile(path.join(__dirname,'public','index.html'))
 })
 
-app.get('/about', (req,res)=>{
-    res.sendFile(path.join(__dirname,'public','about.html'))
+app.get('/crops.html', (req,res)=>{
+    res.sendFile(path.join(__dirname,'public','crops.html'))
 })
+
+app.get('/contact.html', (req,res)=>{
+    res.sendFile(path.join(__dirname,'public','contact.html'))
+})
+
+app.get('/index.html', (req,res)=>{
+    res.sendFile(path.join(__dirname,'public','index.html'))
+})
+
+
 
 
 app.listen(7000,()=>{
